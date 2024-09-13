c6c78a1c973be46d78e6d6ad826ece385d25302e
RAMBAGANESH2425 <ammananna8513@gmail.com>
2024-09-13 22:53:30 +0530

    challange 5
    challange 5

---
+++ leders_in_array.py
@@ -0,0 +1,13 @@
+def find_leaders(arr):
+    n = len(arr)
+    leaders = []
+    max_from_right = arr[-1]
+    leaders.append(max_from_right)
+    for i in range(n-2, -1, -1):
+        if arr[i] > max_from_right:
+            max_from_right = arr[i]
+            leaders.append(max_from_right)
+    return leaders[::-1]
+arr = [16, 17, 4, 3, 5, 2]
+leaders = find_leaders(arr)
+print("Leaders:", leaders)
